d3aa3cd96f4d3b097a4173da76e7744b3e14b7ff
Mariangithubit <metsyb1@hotmail.com>
2023-08-12 14:34:24 -0700

    models

--- beautiful.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# Calculate the area
-
-length = float(input("Please enter the length: "))
-width = float(input("Please enter the width: "))
-
-area = length * width
-
-print("The area is:", area)

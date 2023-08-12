869902b1b4ce8bfa6aa4bf9b4393d0bf9ee48ee4
Mariangithubit <metsyb1@hotmail.com>
2023-08-12 16:01:15 -0700

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Define BaseModel"""
+"""Defines the BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Define the basemodel of the Airbnb"""
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """intialize of Basemodel"""
+        """Initialize a new BaseModel.
 
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -18,21 +22,23 @@ class BaseModel:
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tfrom)
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
                     self.__dict__[k] = v
         else:
-            models.storge.new(self)
+            models.storage.new(self)
 
     def save(self):
-        """ updates the public instance with the current datetime"""
-
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
-        models.storge.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Return to the dictionary of Basemodel"""
+        """Return the dictionary of the BaseModel instance.
 
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
@@ -40,7 +46,6 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return print Basemode in string"""
-
+        """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
